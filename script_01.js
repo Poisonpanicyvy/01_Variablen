4aaaf0a766e5db5a9fd4b7c4b79d79c03c58cd0c
poisonpanicyvy <poisonpanicyvy@web.de>
2023-01-13 14:34:21 +0100

    Neue Variante variable

--- script_01.js
+++ script_01.js
@@ -14,15 +14,22 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName; // Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung
-console.log(firstName); // Ausgabe
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
 
-let familyName = "Mütze";
-console.log(familyName);
-console.log(firstName + " " + familyName);
+// let familyName = "Mütze";
+// console.log(familyName);
+// console.log(firstName + " " + familyName);
 
 // Variable test
 
+let test;
+test = "hallo";
+test = 2;
+test = true;
 
+console.log("Datentyp: " + typeof test);
+
+console.log("Inhalt: " + test);
 
